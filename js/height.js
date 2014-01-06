c0799c07c85e50f6d85fa0aa449273bf2c8fae8a
ccaterinaghost <ccat.x@Hotmail.com>
2014-01-06 17:27:11 -0500

    Added all files

---
+++ js/height.js
@@ -0,0 +1,21 @@
+var colors = [
+  [0, 400, 0],
+  [75, 75, 75],
+  [240, 87, 40],
+  [328, 24, 40]
+],
+
+el = document.getElementsByTagName('body')[0],   // Element to be scrolled
+length = colors.length,                          // Number of colors
+height = Math.round(el.offsetHeight / length);   // Height of the segment between two colors
+
+function scroll() {
+  var i = Math.floor(el.scrollTop / height),     // Start color index
+      d = el.scrollTop % height / height,        // Which part of the segment between start color and end color is passed
+      c1 = colors[i],                            // Start color
+      c2 = colors[(i+1)%length],                 // End color
+      h = c1[0] + Math.round((c2[0] - c1[0]) * d),
+      s = c1[1] + Math.round((c2[1] - c1[1]) * d),
+      l = c1[2] + Math.round((c2[2] - c1[2]) * d);
+  el.style['background-color'] = ['hsl(', h, ', ', s+'%, ', l, '%)'].join('');
+}
